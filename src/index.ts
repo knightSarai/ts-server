6885786fea92a7e69f4a0843bd6b094af39de6d6
knight <knight.sarai.dev@gmail.com>
2021-10-09 16:17:33 +0300

    create basic rout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('knight');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+        <div>
+            <h1>Knight</h1>
+        </div>
+    `);
+});
+
+app.listen('1200', () => console.log('server on localhost:1200'));
