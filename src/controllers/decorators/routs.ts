b98c04e9465e5f4f6f26589480a337c1776983e5
knight <knight.sarai.dev@gmail.com>
2021-10-16 21:51:34 +0300

    Create Factory function routeMethod

--- src/controllers/decorators/routs.ts
+++ src/controllers/decorators/routs.ts
@@ -1,16 +1,18 @@
 import 'reflect-metadata';
+import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
 
 function routeMethod(method: string) {
   return function (path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata('path', path, target, key);
-      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata(MetadataKeys.Path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.Method, method, target, key);
     };
   };
 }
 
-export const get = routeMethod('get');
-export const post = routeMethod('post');
-export const put = routeMethod('put');
-export const del = routeMethod('delete');
-export const patch = routeMethod('patch');
+export const get = routeMethod(Methods.Get);
+export const post = routeMethod(Methods.Post);
+export const put = routeMethod(Methods.Put);
+export const del = routeMethod(Methods.Del);
+export const patch = routeMethod(Methods.Patch);
