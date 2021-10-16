dd6cbf8f90511932e263bc454845093cda7ba234
knight <knight.sarai.dev@gmail.com>
2021-10-16 21:52:59 +0300

    Create use decorator

--- src/controllers/decorators/index.ts
+++ src/controllers/decorators/index.ts
@@ -1,2 +1,3 @@
 export * from './controller';
 export * from './routs';
+export * from './use';
---
+++ src/controllers/decorators/use.ts
@@ -0,0 +1,15 @@
+import { RequestHandler } from 'express';
+import 'reflect-metadata';
+import { MetadataKeys } from './MetadataKeys';
+
+export function use(middleware: RequestHandler) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    const middlewares =
+      Reflect.getMetadata(MetadataKeys.Middleware, target, key) || [];
+    Reflect.defineMetadata(
+      MetadataKeys.Middleware,
+      [...middlewares, middleware],
+      target
+    );
+  };
+}
